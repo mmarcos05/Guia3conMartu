136216d53bdb9fe45176928a436d5ac991077348
lucia <miramonteslucia20@gmail.com>
2024-05-26 18:34:12 -0300

    agrego a carpeta

---
+++ Cami/guia5cami.hs
@@ -0,0 +1,234 @@
+--concatenación es haskell en vez de :: es ++
+--EJERCICIO 1.1
+longitud :: [t] -> Integer
+longitud [] = 0
+longitud (_:xs) = 1 + longitud xs
+
+--EJERCICIO 1.2
+ultimo :: [t] -> t
+ultimo [x] = x
+ultimo (_:xs) = ultimo xs
+
+--EJERCICIO 1.3
+principio :: [t] -> [t]
+principio [x] = []
+principio (x:xs) = x : principio xs
+
+--EJERCICIO 1.4
+reverso :: [t] -> [t]
+reverso [x] = [x]
+reverso (x:xs) = ultimo (x:xs) : (reverso(principio (x:xs)))
+
+--EJERCICIO 2.1
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _[] = False
+pertenece x (y:ys) |x == y = True
+                   |otherwise = pertenece x ys
+                   
+--EJERCICIO 2.2
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [] = False
+todosIguales [x] = True
+todosIguales (x:y:ys) = x == y && todosIguales (y:ys)
+
+--EJERCICIO 2.3
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = False
+todosDistintos [x] = True
+todosDistintos (x:y:ys) = x /= y && todosDistintos (y:ys)
+
+--EJERCICIO 2.4
+--hayRepetidos :: (Eq t) => [t] -> Bool
+--hayRepetidos [] = False
+--hayRepetidos (x:xs)| pertenece x xs = True
+           --        | otherwise = hayRepetidos xs
+
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos [x] = False
+hayRepetidos (x:y:ys) |x == y || hayRepetidos (x:ys) = True
+                      |otherwise = hayRepetidos (y:ys)
+
+--EJERCICIO 2.5
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar x (y:ys) | x == y = ys
+                | otherwise = y : quitar x ys
+
+--EJERCICIO 2.6
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos x (y:ys) |x == y = quitarTodos x ys
+                     |otherwise = y : quitarTodos x ys
+
+--EJERCICIO 2.7
+eliminarRepetidos :: (Eq t) => [t] -> [t] 
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) = x : eliminarRepetidos(quitarTodos x xs)
+
+--EJERCICIO 2.8
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos (x:xs) (y:ys) = todosPertenecen (x:xs) (y:ys) && todosPertenecen (y:ys) (x:xs)
+
+todosPertenecen :: (Eq t) => [t] -> [t] -> Bool
+todosPertenecen [] _ = True
+todosPertenecen (x:xs) (y:ys) = pertenece x (y:ys) && todosPertenecen xs (y:ys)
+
+--EJERCICIO 3.3
+maximo :: [Integer] -> Integer
+maximo [x] = x
+maximo (x:y:ys) |x > y = maximo (x:ys)
+                |y > x = maximo (y:ys)
+                |otherwise = maximo (x:ys)
+
+--EJERCICIO 3.9 
+ordenar :: [Integer] -> [Integer]
+ordenar [] = []
+ordenar [x] = [x]
+ordenar (x:y:ys) = minimo (x:y:ys) : ordenar (quitar (minimo (x:y:ys)) (x:y:ys))
+
+minimo :: [Integer] -> Integer
+minimo [x] = x
+minimo (x:y:ys) |x < y = minimo (x:ys)
+                |y < x = minimo (y:ys)
+                |otherwise = minimo (x:ys)
+
+--EJERCICIO 3.1
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+--EJERCICIO 3.2
+productoria :: [Integer] -> Integer
+productoria [] = 1
+productoria (x:xs) = x*productoria xs
+
+--EJERCICIO 3.3
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN _ [] = []
+sumarN n (x:xs) = n + x : sumarN n xs
+
+--EJERCICIO 3.5
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero [] = []
+sumarElPrimero (x:xs) = sumarN x (x:xs)
+
+--EJERCICIO 3.6
+sumarElUltimo :: [Integer] -> [Integer]
+sumarElUltimo [] = []
+sumarElUltimo (x:xs) = sumarN (ultimo (x:xs)) (x:xs)
+
+--EJERCICIO 4.1
+sacarBlancosRepetidos :: [Char] -> [Char]
+sacarBlancosRepetidos [] = []
+sacarBlancosRepetidos [a] = [a]
+sacarBlancosRepetidos (a:b:bs) |a == ' ' && b == ' ' = sacarBlancosRepetidos (a:bs)
+                               |otherwise = a : sacarBlancosRepetidos (b:bs)
+
+--EJERCICIO 4.2
+contarPalabras :: [Char] -> Integer
+contarPalabras [] = 0
+contarPalabras [a] = 1
+contarPalabras (a:b:bs)|a /= ' ' && b == ' ' = 1 + contarPalabras((sacarEspacioFinal(sacarBlancosRepetidos (b:bs))))
+                       |otherwise = contarPalabras(sacarEspacioFinal(sacarBlancosRepetidos(b:bs)))
+
+sacarEspacioFinal :: [Char] -> [Char]
+sacarEspacioFinal [] = []
+sacarEspacioFinal [a] |a == ' ' = []
+                      |otherwise = [a]
+sacarEspacioFinal (a:as) = a : sacarEspacioFinal as
+
+sacarEspacioInicial :: [Char] -> [Char]
+sacarEspacioInicial [] = []
+sacarEspacioInicial (a:as) |a == ' ' = as
+                           |otherwise = a : sacarEspacioInicial as
+
+--EJERCICIO 4.3
+
+
+primeraPalabra :: [Char] -> [Char]
+primeraPalabra [] = []
+primeraPalabra (a:as) |a == ' ' = []
+                      |otherwise = a : primeraPalabra as
+
+sacarPrimeraPalabra :: [Char] -> [Char]
+sacarPrimeraPalabra [] = []
+sacarPrimeraPalabra (a:as) |a == ' ' = as
+                           |otherwise = sacarPrimeraPalabra as
+
+palabras :: [Char] -> [[Char]]
+palabras [] = []
+palabras (a:as) = primeraPalabra (a:as) : palabras (sacarPrimeraPalabra (sacarBlancosRepetidos(a:as)))
+
+--EJERCICIO 5.1
+{--sumaAcumulada :: (Num t) => [t] -> [t]
+sumaAcumulada [] = []
+sumaAcumulada [x] = [x]
+sumaAcumulada (x:y:ys) = x : sumaAcumulada ((x+y):ys)--}
+
+--EJERCICIO 5.2
+{--descomponerEnPrimos :: [Int] -> [[Int]]
+descomponerEnPrimos [] = []
+descomponerEnPrimos (x:xs) = factoresPrimos x : descomponerEnPrimos xs
+
+factoresPrimos :: Int -> [Int]
+factoresPrimos x | x <= 1 = []
+                 | esPrimo (fromIntegral x) = [x]
+                 | otherwise = menorDivisor x : factoresPrimos (div x (menorDivisor x)) 
+
+menorDivisor :: Int -> Int
+menorDivisor n = menorDivisorDesde n 2
+
+menorDivisorDesde :: Int -> Int -> Int
+menorDivisorDesde n y| n == y = y
+                     | mod n y == 0 = y
+                     | otherwise = menorDivisorDesde n (y+1)
+
+esPrimo :: Int -> Bool
+esPrimo n = menorDivisor n == n --}
+
+--EJERCICIO 5.1 (otra forma)
+
+sumaPrimerosKDigitos :: (Num t, Ord t) => t -> [t] -> t 
+sumaPrimerosKDigitos 0 _ = 0
+sumaPrimerosKDigitos _ [] = 0
+sumaPrimerosKDigitos k (x:xs) = (x + (sumaPrimerosKDigitos (k-1) xs))
+
+listaKDigitos :: (Num t, Ord t) => t -> [t] -> [t]
+listaKDigitos 0 _ = []
+listaKDigitos _ [] = []
+listaKDigitos k (x:xs) = sumaPrimerosKDigitos k (x:xs) : []
+
+sumaAcumuladaAux :: (Num t, Ord t) => t -> [t] -> [t]
+sumaAcumuladaAux _ [] = []
+sumaAcumuladaAux k (x:xs) |k > longitud2 (x:xs) = []
+                          |otherwise = listaKDigitos k (x:xs) ++ sumaAcumuladaAux (k + 1) (x:xs)
+
+sumaAcumulada :: (Num t, Ord t) => [t] -> [t]
+sumaAcumulada (x:xs) = sumaAcumuladaAux 1 (x:xs)
+
+longitud2 :: (Num t, Ord t) => [t] -> t
+longitud2 [] = 0
+longitud2 (_:xs) = 1 + longitud2 xs
+
+--EJERCICIO 5.2 (otra forma)
+menorDivisor :: Int -> Int
+menorDivisor n = menorDivisorDesde n 2
+
+menorDivisorDesde :: Int -> Int -> Int
+menorDivisorDesde n y| n == y = y
+                     | mod n y == 0 = y
+                     | otherwise = menorDivisorDesde n (y+1)
+
+esPrimo :: Int -> Bool
+esPrimo n = menorDivisor n == n 
+
+
+factoreo :: Int -> [Int]
+factoreo n |n <= 1 = []
+           |esPrimo n = [n]
+           |otherwise = menorDivisor n : factoreo (div n (menorDivisor n))
+
+descomponerEnPrimos :: [Int] -> [[Int]]
+descomponerEnPrimos [] = []
+descomponerEnPrimos (x:xs) = [factoreo x] ++ descomponerEnPrimos xs
---
+++ Cami/practica7cami.py
@@ -0,0 +1,117 @@
+#EJERCICIO 1.3
+
+def suma_total(lista:list[int]) -> int:
+    total:int = 0
+    ind_actual:int = 0
+    longitud:int = len(lista)
+    while ind_actual < longitud:
+        total += lista[ind_actual]
+        ind_actual +=1
+    return total
+
+print(suma_total ([1,2,3,4]))
+
+#EJERCICIO 1.1
+
+def pertenece(lista:list[int], numero:int) -> bool:
+    condicion:bool = True
+    i:int = 0
+    longitud:int = len(lista)
+    while i < longitud:
+        if lista[i] == numero:
+            return condicion
+        else:
+            i += 1
+    return False
+
+print(pertenece([1,2,3,4],5))
+
+#EJERCICIO 1.7
+
+def tiene_minuscula(contraseña:str) -> bool:
+    longitud:int = len(contraseña)
+    i:int = 0
+    while i < longitud:
+        if (contraseña[i] >= "a") and (contraseña[i] <= "z"):
+            return True
+        else:
+            i += 1
+    return False
+
+print(tiene_minuscula("HOLA"))
+
+def tiene_mayuscula(contraseña:str) -> bool:
+    longitud:int = len(contraseña)
+    i:int = 0
+    while i < longitud:
+        if (contraseña[i] >= "A") and (contraseña[i] <= "Z"):
+            return True
+        else:
+            i += 1
+    return False
+
+print(tiene_mayuscula("la"))
+
+def tiene_num(contraseña:str) -> bool:
+    longitud:int = len(contraseña)
+    i:int = 0
+    while i < longitud:
+        if (contraseña[i] >= "0") and (contraseña[i] <= "9"):
+            return True
+        else:
+            i += 1
+    return False
+
+print(tiene_num("las"))
+
+def fortaleza(contraseña:str) -> str:
+    longitud:int = len(contraseña)
+    i:int = 0
+    while i < longitud:
+        if (longitud > 8) and (tiene_minuscula(contraseña) == True) and (tiene_mayuscula(contraseña) == True) and (tiene_num(contraseña) == True):
+            return "VERDE"
+        elif longitud < 5:
+            return "ROJA"
+        else:
+            return "AMARILLA"
+        
+print(fortaleza("hola"))
+
+#EJERCICIO 2.1
+def es_par(numero:int) -> bool:
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+
+def borra_pares(lista:list[int]) -> list[int]:
+    i:int = 0
+    longitud:int = len(lista)
+    while i < longitud:
+        if es_par(lista[i]) == True:
+            lista.remove(lista[i])
+            lista.insert(i,0)
+            i += 1
+        else:
+            i += 1
+    return lista
+
+print(borra_pares([1,2,3,4,5,6,7]))
+
+#EJERCICIO 5.2
+
+def pertenece_a_cada_uno_version_2(lista:list[list[int]], e:int) -> list[bool]:
+    i:int = 0
+    longitud:int = len(lista)
+    while i < longitud:
+        if pertenece(lista[i], e) == True:
+            lista.remove(lista[i])
+            lista.insert(i, True)
+            i += 1
+        else:
+            lista.remove(lista[i])
+            lista.insert(i, False)
+            i += 1
+    return lista
+
+print(pertenece_a_cada_uno_version_2([[1,2], [3,4,5,1], [6,7,8]], 1))
