384fc4e9f290092b0fbf56bcd0782a8da0a1569f
mmarcos05 <162136985+mmarcos05@users.noreply.github.com>
2024-05-27 21:07:54 -0300

    practica 7

--- Marqui/practica7.py
+++ Marqui/practica7.py
@@ -78,6 +78,91 @@ def es_par(numero):
     else:
         return False
 
+#Ejercicio 3
+
+def aprobado(notas:list[int]) -> int:
+    if notas_mayores_a_4(notas) and promedio(notas) >= 7:
+        return 1
+    elif notas_mayores_a_4(notas) and promedio(notas) >=4 and promedio(notas) < 7:
+        return 2
+    else:
+        return 3
+
+def notas_mayores_a_4(notas:list[int]) -> bool:
+    i = 0
+    while i < len(notas):
+        if notas[i] >= 4:
+            i +=1
+        else:
+            return False
+    return True
+
+
+def promedio(notas:list[int]) -> float:
+    return (suma_lista(notas) / len(notas)) 
+
+def suma_lista(numeros:list[int]) -> int:
+    i = 0
+    suma = 0
+    while i < len(numeros):
+        suma += numeros[i]
+        i += 1
+    return suma
+
+print(aprobado([8,6,7,10,6]))
+
+#Ejercicio 4.1
+
+def nombres_estudiantes() -> list[str]:
+    lista = []
+    while True:
+        nombre = input("ingrese el nombre del estudiante o ""LISTO"" para finalizar:")
+        if nombre == "LISTO":
+            return lista
+        else:
+            lista.append(nombre)
+
+
+#Ejercicio 4.2
+
+def sube() -> list[tuple]:
+    saldo = 0
+    historial = []
+    while True:
+        operacion = input(f"Su saldo es de {saldo} \nIngrese C para cagar creditos \nIngrese D para descontar creditos \nIngrese X para finalizar la operacion \n")
+        if operacion == "C":
+            carga = int(input("ingrese el monto a cargar:"))
+            historial.append(("C",carga))
+            saldo += carga
+        elif operacion == "D":
+            descontar = int(input(f"ingrese el monto a retirar, su saldo es de {saldo}\n"))
+            if saldo - descontar < 0:
+                print("saldo insuficiente, debe ingresar un monto mas chico")
+            else:
+                historial.append(("D", descontar))
+                saldo -= descontar
+        elif operacion == "X":
+            return f"Ha finalizado su operacion, su saldo es de {saldo} \nEl historial es: {historial}"
+
+#Ejercicio 4.3
+
+#explicacion "in", "inout", "in and out"
+
+# modificar (in edad:N) : N
+
+def modificar(edad:int) -> int:
+    return edad + 5
+
+# modificar (inout edad:N)
+
+def modificar(edad: int) -> None:
+    edad += 5
+
+# modificar (in edad:N, out: otra_edad)
+
+def modificar(edad, otra_edad) -> None:
+    otra_edad = edad + 5
+
 #Ejercicio 5.2
 
 def pertenece_a_cada_uno(s:list[list[int]], e:int) -> list[bool]:
