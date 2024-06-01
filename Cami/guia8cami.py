00a41db04155db33498cd07f0d3ba7b2969b1068
Cami <cami.lamblot@gmail.com>
2024-06-01 01:01:57 -0300

    mas de la 8

--- Cami/guia8cami.py
+++ Cami/guia8cami.py
@@ -1,5 +1,7 @@
 from queue import LifoQueue as Pila
 import random
+import typing
+from typing import List
 
 #EJERCICIO 1.1
 def contar_lineas(nombre_archivo:str) -> int:
@@ -9,6 +11,163 @@ def contar_lineas(nombre_archivo:str) -> int:
     return len(archivo_lineas)
 
 print(contar_lineas("archivo.txt"))
+
+#EJERCICIO 1.2
+def pertenece(a:str, b: List[str]) -> bool:
+    indice: int = 0
+    while indice < len(b):
+        if b[indice] == a:
+            return True
+        else: indice += 1
+    return False
+
+#EJERCICIO 2
+def es_comentario(linea:str) -> bool:
+    i:int = 0
+    while(i < len(linea) and linea[i] == ' '):
+        i += 1
+    return i < len(linea) and linea[i] == '#'
+
+"""def es_comentario(linea:str) -> bool:     está mal no sé por qué
+    longitud:int = len(linea)
+    i:int = 0
+    res:bool = False
+    while (longitud > i):
+        if (linea[i] == ' '):
+            i+=1        
+        else:
+            if (linea[i] == '#'):
+                res = True
+            else:
+                res = False
+    return res"""
+
+
+def clonar_sin_comentarios(nombre_archivo:str) -> None:
+    archivo:typing.IO = open(nombre_archivo, 'r')
+    nuevo_archivo:typing.IO = open("clonado.txt", 'w')
+    lineas:List[str] = archivo.readlines()
+
+    for linea in lineas:
+        if (not es_comentario(linea)):
+            nuevo_archivo.write(linea)
+    nuevo_archivo.close()
+    archivo.close()
+
+    nuevo_archivo = open("clonado.txt", 'r')
+    contenido = nuevo_archivo.read()
+    print(contenido)
+    nuevo_archivo.close()
+
+clonar_sin_comentarios("ejemplo_clonado.py")
+        
+        
+#EJERCICIO 3
+def reversa(lista:List[str]) -> List[str]:    #otra forma de hacer la función de reversa
+    nueva_lista:List[str] = []
+    longitud:int = len(lista)
+    i:int = longitud - 1
+
+    for i in range (longitud-1, (-1), (-1)):
+        nueva_lista.append(lista[i])
+    return nueva_lista
+
+#print(reversa(["cami", "niki", "wendy", "kelly"]))
+
+"""def invertir_lineas(nombre_archivo:str) -> None:
+    archivo:typing.IO = open(nombre_archivo, 'r')
+    nuevo_archivo:typing.IO = open("reverso.txt", 'w')
+
+    lineas:List[str] = archivo.readlines()
+    lineas_reversa:List[str] = reversa(lineas)
+
+    nuevo_archivo.writelines(lineas_reversa)
+
+    archivo.close()
+    nuevo_archivo.close()
+
+    nuevo_archivo = open("reverso.txt", 'r')
+    contenido = nuevo_archivo.read()
+    print(contenido)
+    nuevo_archivo.close()
+
+invertir_lineas("ejemplo_clonado.py")"""
+
+def invertir_lineas(nombre_archivo:str) -> None:
+    archivo:typing.IO = open(nombre_archivo, 'r')
+    nuevo_archivo:typing.IO = open("reverso.txt", 'w')
+    lineas:List[str] = archivo.readlines()
+    cantidad_lineas:int = len(lineas)
+
+    while cantidad_lineas > 0:
+        if pertenece('\n', lineas[cantidad_lineas - 1]):
+            nuevo_archivo.write(lineas[cantidad_lineas - 1])
+        else:
+            nuevo_archivo.write(lineas[cantidad_lineas - 1] + '\n')
+        cantidad_lineas -=1
+    archivo.close()
+    nuevo_archivo.close()
+
+    nuevo_archivo = open("reverso.txt", 'r')
+    contenido = nuevo_archivo.readlines()
+    print(contenido)
+    nuevo_archivo.close()
+
+invertir_lineas("ejemplo_clonado.py")
+
+#EJERCICIO 4
+def agregar_frase_al_final(nombre_archivo:str, frase:str) -> None:
+    archivo:typing.IO = open(nombre_archivo, 'r')
+    lineas:List[str] = archivo.readlines()
+    cantidad_lineas:int = len(lineas)
+    archivo.close()
+
+    archivo:typing.IO = open(nombre_archivo, 'a')
+    
+    if pertenece ('\n', lineas[cantidad_lineas - 1]):
+        archivo.write(frase)
+    else:
+        archivo.write('\n' + frase)
+    archivo.close()
+
+    archivo = open(nombre_archivo, 'r')
+    contenido = archivo.read()
+    print(contenido)
+    archivo.close()
+
+#agregar_frase_al_final("archivo.txt", "esta es una prueba")
+
+#EJERCICIO 5
+"""def agregar_frase_al_principio(nombre_archivo:str, frase:str) -> None:
+    archivo:typing.IO = open(nombre_archivo, 'r')
+    lineas:List[str] = archivo.readlines()
+    cantidad_lineas:int = len(lineas)
+    archivo.close()
+    
+    archivo:typing.IO = open(nombre_archivo, 'w')
+    archivo.write(lineas[cantidad_lineas - 1])
+    archivo.close()
+
+    archivo:typing.IO = open(nombre_archivo, 'r')
+    lineas:List[str] = archivo.readlines()
+    cantidad_lineas:int = len(lineas)
+    archivo.close()
+
+    archivo:typing.IO = open(nombre_archivo, 'a')
+
+    while cantidad_lineas >= 0:
+        lineas[cantidad_lineas - 2] = lineas[cantidad_lineas - 3]
+        cantidad_lineas -= 1
+    lineas[0] = frase
+    archivo.close()
+
+    archivo = open(nombre_archivo, 'r')
+    contenido = archivo.read()
+    print(contenido)
+    archivo.close()
+
+agregar_frase_al_principio("archivo.txt", "esta es una prueba")"""
+
 
 #EJERCICIO 8
 def generar_nros_al_azar(cantidad: int, desde:int, hasta:int) -> Pila[int]:
